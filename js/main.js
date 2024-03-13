dbdf717cfc08a3a79cd97c793b79dd34cb1b5a7b
Dcarolinamorenoc <156273221+Dcarolinamorenoc@users.noreply.github.com>
2024-03-13 01:42:09 -0500

    feat: :sparkles: Proyecto web parte 1 terminado
    Como ultimo avance hice un documento con js que me permitiera desplegar mi menu en el responsive

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function toggleMenu(){ 
+    const menuToggle = document.querySelector('.toggle'); 
+    const caja2 = document.querySelector('.caja2'); 
+    menuToggle.classList.toggle('active') 
+    caja2.classList.toggle('active') 
+  }
